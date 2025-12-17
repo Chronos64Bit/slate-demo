598c3a57c22caa08be14ce5f36002f6c9a044fb4
Mark <chronos.software.x64@gmail.com>
2025-12-17 13:04:24 +0000

    Update generated code via Slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-body {
-  background-color: #0a0a0a;
-  color: #e5e5e5;
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
